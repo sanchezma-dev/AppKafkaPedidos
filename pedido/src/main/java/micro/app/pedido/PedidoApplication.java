230c287d1613540d1a4eb86894485f6a83dd73cf
Josema <sanchezma.dev@gmail.com>
2024-01-05 15:42:43 +0100

    Microservicios usuario y pedido

---
+++ pedido/src/main/java/micro/app/pedido/PedidoApplication.java
@@ -0,0 +1,13 @@
+package micro.app.pedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidoApplication.class, args);
+	}
+
+}
