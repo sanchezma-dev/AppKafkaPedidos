00fc31f4275116aaef3d3ec537a748ca4e66950e
Josema <sanchezma.dev@gmail.com>
2024-01-05 16:06:14 +0100

    Microservicios de logisitica y notificacion

---
+++ notificacion/src/test/java/micro/app/notificacion/NotificacionApplicationTests.java
@@ -0,0 +1,13 @@
+package micro.app.notificacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
