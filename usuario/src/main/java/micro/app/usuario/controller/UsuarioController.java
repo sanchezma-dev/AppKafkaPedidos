03aa727e3fb443beb8f0d47a38dd3cff207fbd3f
Josema <sanchezma.dev@gmail.com>
2024-01-08 18:34:51 +0100

    Cambios nuevos

--- usuario/src/main/java/micro/app/usuario/controller/UsuarioController.java
+++ usuario/src/main/java/micro/app/usuario/controller/UsuarioController.java
@@ -1,10 +1,9 @@
 package micro.app.usuario.controller;
 
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import micro.app.usuario.dto.UsuarioDto;
 import micro.app.usuario.service.IUsuarioService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,12 +12,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping(value = "/usuario")
 public class UsuarioController {
 
-    /** Log de usuarioController */
-    private static final Logger LOG = LoggerFactory.getLogger(UsuarioController.class);
 
     /** Servicio usuario */
     @Autowired
@@ -26,7 +24,7 @@ public class UsuarioController {
 
     @PostMapping(value = "/alta")
     public ResponseEntity<?> alta (@Valid @RequestBody final UsuarioDto usuarioDto) {
-        LOG.info("Endpoint UsuarioController.alta");
+        log.info("Entrando en alta usuarioController");
         return ResponseEntity.status(HttpStatus.OK).body(service.saveUsuario(usuarioDto));
     }
 
