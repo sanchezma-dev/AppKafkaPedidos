c6b8c4d42d7fc351f37337d724e0aaf7d2836105
Josema <sanchezma.dev@gmail.com>
2024-01-18 18:10:57 +0100

    Nuevos cambios

--- pedido/src/main/java/micro/app/pedido/entity/PedidoEntity.java
+++ pedido/src/main/java/micro/app/pedido/entity/PedidoEntity.java
@@ -6,13 +6,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "Pedidos")
+@Table(name = "pedidos")
 public class PedidoEntity {
 
     @Id
@@ -20,13 +22,20 @@ public class PedidoEntity {
     @Column(name = "ID", nullable = false)
     private Long id;
 
-    @Column(name = "idUsuario", nullable = false)
+    @Column(name = "ID_USUARIO", nullable = false)
     private String idUsuario;
 
-    @Column(name = "fecha", nullable = false)
+    @Column(name = "FECHA", nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
     private Date fecha;
 
-    @Column(name = "estado", nullable = false)
+    @Column(name = "ESTADO", nullable = false)
     private String estado;
+
+    // Relacion unidireccional. Un pedido puede tener varios productos
+    // No se pone el campo ID_PEDIDO en el entity ProductoPedidoEntity
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "ID_PEDIDO")
+    private List<ProductoPedidoEntity> productos = new ArrayList<>();
 
 }
--- pedido/src/main/java/micro/app/pedido/entity/ProductoPedidoEntity.java
+++ pedido/src/main/java/micro/app/pedido/entity/ProductoPedidoEntity.java
@@ -1,4 +1,22 @@
 package micro.app.pedido.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "pedidos_productos")
 public class ProductoPedidoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID", nullable = false)
+    private Long id;
+
+    @Column(name = "ID_PRODUCTO", nullable = false)
+    private Long idProducto;
 }
