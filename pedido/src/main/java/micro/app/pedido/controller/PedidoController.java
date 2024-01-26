7e01bb2e066ffade231668b15c21022918a32f89
Josema <sanchezma.dev@gmail.com>
2024-01-26 16:25:20 +0100

    Nuevas correcciones 2

--- pedido/src/main/java/micro/app/pedido/controller/PedidoController.java
+++ pedido/src/main/java/micro/app/pedido/controller/PedidoController.java
@@ -1,31 +1,33 @@
 package micro.app.pedido.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import micro.app.pedido.dto.PedidoDto;
 import micro.app.pedido.dto.Solicitud;
-import micro.app.pedido.entity.PedidoEntity;
+import micro.app.pedido.exceptions.ApiResponseException;
 import micro.app.pedido.service.IPedidoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
 @RequestMapping(value = "/pedido")
 public class PedidoController {
 
-    /** Servicio usuario */
+    /** Servicio pedido */
     @Autowired
     private IPedidoService service;
 
     @PostMapping(value = "/alta")
-    public ResponseEntity<?> alta (@RequestBody final Solicitud solicitud) {
+    @ResponseBody // Indica que la salida sera en JSON, como si fuera produces = "application/json"
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<?> alta (@RequestBody final Solicitud solicitud) { //ResquestBody = consumes = "application/json"
         log.info("Entrando en alta PedidoController.alta");
-        service.altaPedido(solicitud);
-        return new ResponseEntity<>("Usuario guardado con éxito", HttpStatus.OK);
+        try{
+            service.altaPedido(solicitud);
+            return new ResponseEntity<>("El pedido se ha guardado con éxito", HttpStatus.OK);
+        }catch (ApiResponseException e){
+            return ResponseEntity.status(e.getHttpStatus()).body(e.getMessage());
+        }
     }
 }
